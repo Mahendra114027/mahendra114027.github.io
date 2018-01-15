a1c7fd70269bef0fcb16e78297c3e7d7968acaf8
Mahendra114027 <samcmvs11225@gmail.com>
2018-01-15 20:37:35 +0530

    Link update and minor changes for color scheme

--- js/pagination.js
+++ js/pagination.js
@@ -6,8 +6,8 @@ function openTab(evt, cityName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-amber", "");
+      tablinks[i].className = tablinks[i].className.replace(" w3-green", "");
   }
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " w3-amber";
+  evt.currentTarget.className += " w3-green";
 }
